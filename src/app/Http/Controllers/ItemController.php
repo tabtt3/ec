8027f3702db3757c82628aeed568751222d11545
tabtt3 <tabttppp@gmail.com>
2020-12-12 23:52:22 +0900

    feat: add item index page

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
+    public function index()
+    {
+        $items = Item::all();
+
+        return view('items.index')->with([
+            'items' => $items
+        ]);
+    }
+
     public function show(int $id)
     {
         $item = Item::findOrFail($id);
